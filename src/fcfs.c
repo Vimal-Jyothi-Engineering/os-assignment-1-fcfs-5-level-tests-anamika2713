fdb8081a812315e73e613a2b237b3af7cd2ae27b
anamika2713 <anamikashimjith27@gmail.com>
2026-03-01 06:47:07 +0530

    Update fcfs.c

--- src/fcfs.c
+++ src/fcfs.c
@@ -1,27 +1,71 @@
 #include <stdio.h>
-#include <string.h>
-
-typedef struct {
-    char pid[10];
-    int arrival;
-    int burst;
-    int waiting;
-    int turnaround;
-} Process;
 
 int main() {
     int n;
+    
+    // Read number of processes
     scanf("%d", &n);
-
-    Process p[n];
-
-    for (int i = 0; i < n; i++) {
-        scanf("%s %d %d", p[i].pid, &p[i].arrival, &p[i].burst);
+    
+    int pid[n], at[n], bt[n], wt[n], tat[n];
+    
+    // Read PID, Arrival Time, Burst Time
+    for(int i = 0; i < n; i++) {
+        scanf("%d %d %d", &pid[i], &at[i], &bt[i]);
     }
-
-    // TODO: Sort by arrival time
-    // TODO: Compute waiting & turnaround time
-    // TODO: Print EXACTLY in required format
-
+    
+    // Sort by Arrival Time (FCFS)
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(at[i] > at[j]) {
+                int temp;
+                
+                temp = at[i];
+                at[i] = at[j];
+                at[j] = temp;
+                
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+                
+                temp = pid[i];
+                pid[i] = pid[j];
+                pid[j] = temp;
+            }
+        }
+    }
+    
+    // Calculate Waiting Time and Turnaround Time
+    int current_time = 0;
+    float total_wt = 0, total_tat = 0;
+    
+    for(int i = 0; i < n; i++) {
+        
+        if(current_time < at[i]) {
+            current_time = at[i];
+        }
+        
+        wt[i] = current_time - at[i];
+        current_time += bt[i];
+        tat[i] = wt[i] + bt[i];
+        
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+    
+    // Print EXACT required format
+    
+    printf("Waiting Time:\n");
+    for(int i = 0; i < n; i++) {
+        printf("P%d %d\n", pid[i], wt[i]);
+    }
+    
+    printf("Turnaround Time:\n");
+    for(int i = 0; i < n; i++) {
+        printf("P%d %d\n", pid[i], tat[i]);
+    }
+    
+    printf("Average Waiting Time: %.2f\n", total_wt / n);
+    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+    
     return 0;
 }
