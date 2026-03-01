07afe98db21c679ecc65ce34f518eeb65663bec9
anamika2713 <anamikashimjith27@gmail.com>
2026-03-01 12:48:14 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -6,12 +6,11 @@ int main() {
 
     int pid[n], at[n], bt[n], wt[n], tat[n];
 
-    // Read PID, Arrival Time, Burst Time
     for (int i = 0; i < n; i++) {
         scanf("%d %d %d", &pid[i], &at[i], &bt[i]);
     }
 
-    // Sort by Arrival Time (FCFS)
+    // Sort by arrival time
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
             if (at[i] > at[j]) {
@@ -35,7 +34,6 @@ int main() {
     int current_time = 0;
     float total_wt = 0, total_tat = 0;
 
-    // Calculate WT and TAT
     for (int i = 0; i < n; i++) {
 
         if (current_time < at[i])
@@ -49,22 +47,24 @@ int main() {
         total_tat += tat[i];
     }
 
-    // EXACT required output format
+    // EXACT FORMAT (NO EXTRA SPACES OR LINES)
 
     printf("Waiting Time: ");
     for (int i = 0; i < n; i++) {
-        printf("P%d %d ", pid[i], wt[i]);
+        printf("P%d %d", pid[i], wt[i]);
+        if (i != n - 1) printf(" ");
     }
     printf("\n");
 
     printf("Turnaround Time: ");
     for (int i = 0; i < n; i++) {
-        printf("P%d %d ", pid[i], tat[i]);
+        printf("P%d %d", pid[i], tat[i]);
+        if (i != n - 1) printf(" ");
     }
     printf("\n");
 
     printf("Average Waiting Time: %.2f\n", total_wt / n);
-    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+    printf("Average Turnaround Time: %.2f", total_tat / n);
 
     return 0;
 }
